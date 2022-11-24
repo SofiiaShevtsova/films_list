3bab5a667251a1bf38076e8f0d438290c03084cc
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2022-11-24 23:14:08 +0200

    find posters for films

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,25 @@
 import "../../node_modules/modern-normalize";
+const axios = require("axios").default;
+
+const boxForFilms = document.querySelector(".films__box")
+
+export const showFilmsList = {
+    options:
+      "api_key=ba9af9187d823167244a35c2fd918141",
+    baseUrl: "https://api.themoviedb.org/3/trending/all/day",
+  
+    async getFilms() {
+  
+      try {
+        const response = await axios.get(
+          `${this.baseUrl}?${this.options}`
+        );
+        console.log(response.data.results.map(e=>{
+            console.log(`https://image.tmdb.org/t/p/w500/${e.poster_path}`);
+        }));
+      } catch (error) {}
+  
+    },
+  };
+
+  showFilmsList.getFilms()
