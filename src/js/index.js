c88b06639691217d0fedd7cbaebea119ff8acf81
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2022-11-25 21:24:41 +0200

    do all reqvest

--- src/js/index.js
+++ src/js/index.js
@@ -2,61 +2,56 @@ import "../../node_modules/modern-normalize";
 const axios = require("axios").default;
 import { getTemplate } from "./templates";
 
+const boxForFilms = document.querySelector(".films__box");
 
-const boxForFilms = document.querySelector(".films__box")
+export const genres = {
+  options: "api_key=ba9af9187d823167244a35c2fd918141",
+  baseUrl: "https://api.themoviedb.org/3/genre/movie/list?language=en-US&",
+  name: {},
 
+  async getGenres() {
+    try {
+      const response = await axios.get(`${this.baseUrl}${this.options}`);
 
-const showGenresById = {
-  options:
-  "api_key=ba9af9187d823167244a35c2fd918141",
-baseUrl: "https://api.themoviedb.org/3/genre/movie/list?language=en-US&",
-genresNameById:[],
+      const genresNameById = [...response.data.genres];
 
+      for (const elem of genresNameById) {
+        const id = Object.values(elem)[0];
+        const name = Object.values(elem)[1];
 
-async getGenres() {
-  try {
+        this.name = { ...this.name, [id]: name };
+      }
+    } catch (error) {}
+  },
+};
 
-    const response = await axios.get(
-      `${this.baseUrl}${this.options}`
-    );
-    this.genresNameById.push(...response.data.genres)
-    // console.log(this.genresNameById);
-  } catch (error) {}
-},
-findName(id){
-  console.log(this.genresNameById);
-  for (const genre of this.genresNameById) {
-    console.log(genre.id);
-  }
-  
-
-  
-  
-    
-  
-}
-}
-
-showGenresById.getGenres()
-showGenresById.findName()
+genres.getGenres();
 
 const showFilmsList = {
-    options:
-      "api_key=ba9af9187d823167244a35c2fd918141",
-    baseUrl: "https://api.themoviedb.org/3/trending/all/day",
-  
-    async getFilms() {
-      try {
-        const response = await axios.get(
-          `${this.baseUrl}?${this.options}`
-        );
-       const markupFilms = response.data.results.map(e=>{
-          return getTemplate(e)
-        }).join("");
-        boxForFilms.innerHTML = markupFilms
-      } catch (error) {}
-  
-    },
-  };
+  options: "api_key=ba9af9187d823167244a35c2fd918141",
+  baseUrl: "https://api.themoviedb.org/3/trending/all/day",
 
-  showFilmsList.getFilms()
+  async getFilms() {
+    try {
+      const response = await axios.get(`${this.baseUrl}?${this.options}`);
+
+      const markupFilms = response.data.results
+        .map((e) => {
+          const genresName = [];
+          for (const elem of e.genre_ids) {
+            if (!genres.name[`${elem}`]) {
+              continue;
+            }
+            genresName.push(genres.name[`${elem}`]);
+          }
+
+          return getTemplate(e, genresName);
+        })
+        .join("");
+
+      boxForFilms.innerHTML = markupFilms;
+    } catch (error) {}
+  },
+};
+
+showFilmsList.getFilms();
--- src/js/templates.js
+++ src/js/templates.js
@@ -1,9 +1,15 @@
-export const getTemplate = function (e) {
-  const template = `<li>
+import { genres } from "./index";
+
+export const getTemplate = function (e, genresName) {
+  const genre = genresName.slice(0, 3).join(",");
+
+  const template = `<li id="${e.id}">
 <img src="https://image.tmdb.org/t/p/w500/${e.poster_path}" alt="film">
-<h2>${e.title||e.name}</h2>
-<p>${e.genre_ids}</p>
+<h2>${e.title || e.name}</h2>
+<p>${genre}</p>
 <p>${e.release_date}</p>
 <p>${e.vote_average}</p>
 </li>`;
-return template} ;
+  // console.log(template);
+  return template;
+};
